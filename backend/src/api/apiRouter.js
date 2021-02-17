d9e32840d1ed9af133e7c356b1acc6e9b18e2a42
Cristina Matei <Cristina.Matei@softwire.com>
2021-02-17 14:38:06 +0200

    Minor fix

--- backend/src/api/apiRouter.js
+++ backend/src/api/apiRouter.js
@@ -1,9 +1,9 @@
 import { ApiRouterBuilder } from './apiRouterBuilder.js'
-import { getUserEndpoint } from './endpoints/addUser.js'
+import { createUserEndpoint } from './endpoints/addUser.js'
 import { exampleEndpoint } from './endpoints/exampleEndpoint.js'
 
 const routerBuilder = new ApiRouterBuilder()
 routerBuilder.addGetEndpoint('/', exampleEndpoint)
-routerBuilder.addGetEndpoint('/users', getUserEndpoint)
+routerBuilder.addGetEndpoint('/users', createUserEndpoint)
 
 export const apiRouter = routerBuilder.router
